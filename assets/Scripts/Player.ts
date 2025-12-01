cddf1c2228c6ba498a148391c2a5bd6f947d0327
ric Cheng <cl252738669@163.com>
2025-12-01 14:34:27 +0800

    控制player移动

--- assets/Scripts/Player.ts
+++ assets/Scripts/Player.ts
@@ -1,14 +1,40 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, EventMouse, EventTouch, Input, input, Node, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
 export class Player extends Component {
-    start() {
 
+    protected onLoad(): void {
+        input.on(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
     }
 
-    update(deltaTime: number) {
-        //``
+    protected onDestroy(): void {
+        input.off(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
+    }
+
+    onTouchMove(event: EventTouch) {
+        let delta = event.getDelta();
+
+        let targetPos = new Vec3();
+
+        if (this.node.position.x + delta.x < -230) {
+            targetPos.x = -230;
+        } else if (this.node.position.x + delta.x > 230) {
+            targetPos.x = 230;
+        } else {
+            targetPos.x = this.node.position.x + delta.x;
+        }
+
+        if (this.node.position.y + delta.y < -370) {
+            targetPos.y = -370;
+        } else if (this.node.position.y + delta.y > 370) {
+            targetPos.y = 370;
+        } else {
+            targetPos.y = this.node.position.y + delta.y;
+        }   
+
+        this.node.setPosition(targetPos);
+
     }
 }
 
