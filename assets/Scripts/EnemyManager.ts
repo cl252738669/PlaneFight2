b4cd70f1377c3a20d065cabb0ecb955a59e5de48
ric Cheng <cl252738669@163.com>
2025-12-01 17:06:58 +0800

    敌人自动生成,移动

---
+++ assets/Scripts/EnemyManager.ts
@@ -0,0 +1,62 @@
+import { _decorator, Component, instantiate, math, Node, Prefab } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('EnemyManager')
+export class EnemyManager extends Component {
+
+    @property
+    enemy0SpawnTime: number = 1;
+    @property(Prefab)
+    enemy0Prefab: Prefab = null;
+
+    @property
+    enemy1SpawnTime: number = 2;
+    @property(Prefab)
+    enemy1Prefab: Prefab = null;
+
+    @property
+    enemy2SpawnTime: number = 7;
+    @property(Prefab)
+    enemy2Prefab: Prefab = null;
+
+    start() {
+        // this.schedule(() => {
+        //     this.enemy0Spawn();
+        // }, this.enemy0SpawnTime);
+
+        this.schedule(this.enemy0Spawn, this.enemy0SpawnTime);
+        this.schedule(this.enemy1Spawn, this.enemy1SpawnTime);
+        this.schedule(this.enemy2Spawn, this.enemy2SpawnTime);
+
+    }
+
+    protected onDestroy(): void {
+        this.unschedule(this.enemy0Spawn);
+    }
+
+    update(deltaTime: number) {
+        
+    }
+
+    enemy0Spawn() {
+        const enemy = instantiate(this.enemy0Prefab);
+        this.node.addChild(enemy);
+        const x = math.randomRangeInt(-205, 205);
+        enemy.setPosition(x, 450);
+    }
+
+    enemy1Spawn() {
+        const enemy = instantiate(this.enemy1Prefab);
+        this.node.addChild(enemy);
+        const x = math.randomRangeInt(-200, 200);
+        enemy.setPosition(x, 480);
+    }
+
+    enemy2Spawn() {
+        const enemy = instantiate(this.enemy2Prefab);
+        this.node.addChild(enemy);
+        const x = math.randomRangeInt(-150, 150);
+        enemy.setPosition(x, 555);
+    }
+}
+
